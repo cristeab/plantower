03374742eb327249f83513ce3324eb22852bd5ca
Bogdan Cristea <cristeab@gmail.com>
2025-01-02 20:58:32 +0200

    add automatic ttyACM port detection

--- test.py
+++ test.py
@@ -6,6 +6,7 @@
 from argparse import ArgumentParser
 import time
 import plantower
+from utils import find_serial_port
 
 
 PARSER = ArgumentParser(
@@ -17,12 +18,18 @@ PARSER.add_argument(
 ARGS = PARSER.parse_args()
 
 #  test code for active mode
-PLANTOWER = plantower.Plantower(port=ARGS.port)
+serial_port = find_serial_port()
+PLANTOWER = plantower.Plantower(serial_port)
 print("Making sure it's correctly setup for active mode. Please wait")
 #make sure it's in the correct mode if it's been used for passive beforehand
 #Not needed if freshly plugged in
 PLANTOWER.mode_change(plantower.PMS_ACTIVE_MODE) #change back into active mode
 PLANTOWER.set_to_wakeup() #ensure fan is spinning
 time.sleep(30) # give it a chance to stabilise
+
+new_serial_port = find_serial_port()
+if new_serial_port != serial_port:
+    PLANTOWER = plantower.Plantower(new_serial_port)
+
 #actually do the reading
 print(PLANTOWER.read())
