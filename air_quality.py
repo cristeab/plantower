575f70a06070c803ac15928f5059de32060fac51
Bogdan Cristea <cristeab@gmail.com>
2025-01-12 21:52:48 +0200

    add class to compute AQI

--- air_quality.py
+++ air_quality.py
@@ -3,138 +3,30 @@
     Basic test script to demonstrate active mode of the plantower
 """
 
-import time
-import plantower
-from utils import find_serial_port
-from collections import deque
-from statistics import mean
-from datetime import timedelta
-import threading
+from air_quality_utils import AirQualityUtils
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import threading
+from statistics import mean
+import time
 
 
-enable_active_mode = True
+aq_utils = AirQualityUtils()
+
 matplotlib.use('TkAgg')
-#  test code for active mode
-serial_port = find_serial_port()
-PLANTOWER = plantower.Plantower(serial_port)
-
 plt.ion()  # Turn on interactive mode for real-time updates
 fig, (ax, ax_bar) = plt.subplots(2, 1, figsize=(10, 8))
 
-if enable_active_mode:
-    print("Making sure it's correctly setup for active mode. Please wait")
-    #make sure it's in the correct mode if it's been used for passive beforehand
-    #Not needed if freshly plugged in
-    PLANTOWER.mode_change(plantower.PMS_ACTIVE_MODE) #change back into active mode
-    PLANTOWER.set_to_wakeup() #ensure fan is spinning
-    time.sleep(30) # give it a chance to stabilise
-
-    new_serial_port = find_serial_port()
-    if new_serial_port != serial_port:
-        PLANTOWER = plantower.Plantower(new_serial_port)
-
-# Initialize empty lists to store time and data
-max_length = 100
-time_data = deque(maxlen=max_length)
-
-pm1_cf1 = deque(maxlen=max_length)
-pm2_5_cf1 = deque(maxlen=max_length)
-pm10_cf1 = deque(maxlen=max_length)
-
-pm1_std = deque(maxlen=max_length)
-pm2_5_std = deque(maxlen=max_length)
-pm10_std = deque(maxlen=max_length)
-
-particle_counts = {
-    ">0.3μm": deque(maxlen=max_length),
-    ">0.5μm": deque(maxlen=max_length),
-    ">1.0μm": deque(maxlen=max_length),
-    ">2.5μm": deque(maxlen=max_length),
-    ">5.0μm": deque(maxlen=max_length),
-    ">10μm": deque(maxlen=max_length),
-}
-sizes = list(particle_counts.keys())
-
-# Initialize a deque to store PM2.5 readings and their timestamps
-pm_readings = deque()
-timestamps = deque()
-
-def add_pm25_reading(current_time, value):
-    pm_readings.append(value)
-    timestamps.append(current_time)
-
-    # Remove readings older than 10 minutes (600 seconds)
-    while timestamps and current_time - timestamps[0] > timedelta(seconds=600):
-        pm_readings.popleft()
-        timestamps.popleft()
-
-def calculate_nowcast_aqi():
-    if len(pm_readings) < 2:
-        return None
-
-    # Calculate the range and scaled rate of change
-    min_pm = min(pm_readings)
-    max_pm = max(pm_readings)
-    range_pm = max_pm - min_pm
-    scaled_rate_of_change = range_pm / max_pm if max_pm != 0 else 0
-
-    # Calculate weight factor
-    weight_factor = max(1 - scaled_rate_of_change, 0.5)
-
-    # Calculate weighted average concentration
-    weighted_sum = 0
-    weight_sum = 0
-    for i, (pm, time) in enumerate(zip(pm_readings, timestamps)):
-        time_diff = (timestamps[-1] - time).total_seconds() / 600  # Normalize to 10 minutes
-        weight = weight_factor ** time_diff
-        weighted_sum += pm * weight
-        weight_sum += weight
-
-    nowcast_concentration = weighted_sum / weight_sum
-
-    # AQI breakpoints for PM2.5
-    breakpoints = [
-        (0.0, 12.0, 0, 50),
-        (12.1, 35.4, 51, 100),
-        (35.5, 55.4, 101, 150),
-        (55.5, 150.4, 151, 200),
-        (150.5, 250.4, 201, 300),
-        (250.5, 500.4, 301, 500)
-    ]
-
-    for bp_low, bp_high, aqi_low, aqi_high in breakpoints:
-        if bp_low <= nowcast_concentration <= bp_high:
-            aqi = ((aqi_high - aqi_low) / (bp_high - bp_low)) * (nowcast_concentration - bp_low) + aqi_low
-            return round(aqi, 1)
-
-    return None  # If concentration is out of range
-
-def aqi_category(aqi):
-    if 0 <= aqi:
-        if aqi <= 50:
-            return 'Good'
-        if aqi <= 100:
-            return 'Moderate'
-        if aqi <= 150:
-            return 'Unhealthy for Sensitive Groups'
-        if aqi <= 200:
-            return 'Unhealthy'
-        if aqi <= 300:
-            return 'Very Unhealthy'
-        if aqi <= 500:
-            return 'Hazardous'
-    return 'Out of range AQI {aqi}'
 
 def continuous_update():
     while True:
-        aqi = calculate_nowcast_aqi()
+        aqi = aq_utils.calculate_nowcast_aqi()
         if aqi is None:
             continue
-        print(f"\rReal-Time Time Series of PM Concentrations, AQI: {aqi:.2f} ({aqi_category(aqi)})", end="", flush=True)
+        print(f"\rReal-Time Time Series of PM Concentrations, AQI: {aqi:.2f} ({AirQualityUtils.aqi_category(aqi)})", end="", flush=True)
         time.sleep(1)  # Update every second
+
 
 # Start a thread to continuously update the PM2.5 average
 update_thread = threading.Thread(target=continuous_update, daemon=True)
@@ -163,48 +55,25 @@ ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
 plt.tight_layout()
 
 #actually do the reading
-sample_count = 0
 print("Start reading data")
 try:
     while True:
-        sample = PLANTOWER.read()
-        sample_count += 1
-
-        # Append new data to the lists
-        time_data.append(sample.timestamp)
-
-        pm1_cf1.append(sample.pm10_cf1)
-        pm2_5_cf1.append(sample.pm25_cf1)
-        pm10_cf1.append(sample.pm100_cf1)
-
-        pm1_std.append(sample.pm10_std)
-        pm2_5_std.append(sample.pm25_std)
-        pm10_std.append(sample.pm100_std)
-
-        particle_counts[">0.3μm"].append(sample.gr03um)
-        particle_counts[">0.5μm"].append(sample.gr05um)
-        particle_counts[">1.0μm"].append(sample.gr10um)
-        particle_counts[">2.5μm"].append(sample.gr25um)
-        particle_counts[">5.0μm"].append(sample.gr50um)
-        particle_counts[">10μm"].append(sample.gr100um)
-
-        # update PM data for AQI computation
-        add_pm25_reading(sample.timestamp, sample.pm25_cf1)
+        aq_utils.read_sample()
 
         # Update the plot data
-        line_pm1_cf1.set_xdata(time_data)
-        line_pm1_cf1.set_ydata(pm1_cf1)
-        line_pm2_5_cf1.set_xdata(time_data)
-        line_pm2_5_cf1.set_ydata(pm2_5_cf1)
-        line_pm10_cf1.set_xdata(time_data)
-        line_pm10_cf1.set_ydata(pm10_cf1)
+        line_pm1_cf1.set_xdata(aq_utils.time_data)
+        line_pm1_cf1.set_ydata(aq_utils.pm1_cf1)
+        line_pm2_5_cf1.set_xdata(aq_utils.time_data)
+        line_pm2_5_cf1.set_ydata(aq_utils.pm2_5_cf1)
+        line_pm10_cf1.set_xdata(aq_utils.time_data)
+        line_pm10_cf1.set_ydata(aq_utils.pm10_cf1)
 
-        line_pm1_std.set_xdata(time_data)
-        line_pm1_std.set_ydata(pm1_std)
-        line_pm2_5_std.set_xdata(time_data)
-        line_pm2_5_std.set_ydata(pm2_5_std)
-        line_pm10_std.set_xdata(time_data)
-        line_pm10_std.set_ydata(pm10_std)
+        line_pm1_std.set_xdata(aq_utils.time_data)
+        line_pm1_std.set_ydata(aq_utils.pm1_std)
+        line_pm2_5_std.set_xdata(aq_utils.time_data)
+        line_pm2_5_std.set_ydata(aq_utils.pm2_5_std)
+        line_pm10_std.set_xdata(aq_utils.time_data)
+        line_pm10_std.set_ydata(aq_utils.pm10_std)
 
         # Adjust plot limits dynamically
         ax.relim()
@@ -215,8 +84,8 @@ try:
         fig.canvas.flush_events()
 
         # Plot particle counts for different size ranges
-        ax_bar.set_title(f'Particle Count Distribution ({sample_count})')
-        counts = [mean(particle_counts[size]) if len(particle_counts[size]) > 0 else 0 for size in sizes]
-        ax_bar.bar(sizes, counts, color=['blue', 'green', 'red', 'purple', 'orange', 'brown'])
+        ax_bar.set_title(f'Particle Count Distribution ({aq_utils.sample_count})')
+        counts = [mean(aq_utils.particle_counts[size]) if len(aq_utils.particle_counts[size]) > 0 else 0 for size in aq_utils.PARTICLE_SIZES]
+        ax_bar.bar(aq_utils.PARTICLE_SIZES, counts, color=['blue', 'green', 'red', 'purple', 'orange', 'brown'])
 except KeyboardInterrupt:
     print("Real-time plotting stopped.")
---
+++ air_quality_utils.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python3
+
+import serial.tools.list_ports
+import sys
+import plantower
+import time
+from collections import deque
+from datetime import timedelta
+from threading import Lock
+
+
+class AirQualityUtils:
+    ENABLE_ACTIVE_MODE = True
+    MAX_QUEUE_LENGTH = 100
+    MEASUREMENT_WINDOW_LENGTH_SEC = 600 # 10 minutes
+    WAKEUP_DELAY_SEC = 30
+
+    # AQI breakpoints for PM2.5
+    BREAKPOINTS = [
+                (0.0, 12.0, 0, 50),
+                (12.1, 35.4, 51, 100),
+                (35.5, 55.4, 101, 150),
+                (55.5, 150.4, 151, 200),
+                (150.5, 250.4, 201, 300),
+                (250.5, 500.4, 301, 500)
+            ]
+
+    sample_count = 0
+    time_data = deque(maxlen=MAX_QUEUE_LENGTH)
+
+    pm1_cf1 = deque(maxlen=MAX_QUEUE_LENGTH)
+    pm2_5_cf1 = deque(maxlen=MAX_QUEUE_LENGTH)
+    pm10_cf1 = deque(maxlen=MAX_QUEUE_LENGTH)
+
+    pm1_std = deque(maxlen=MAX_QUEUE_LENGTH)
+    pm2_5_std = deque(maxlen=MAX_QUEUE_LENGTH)
+    pm10_std = deque(maxlen=MAX_QUEUE_LENGTH)
+
+    particle_counts = {
+        ">0.3μm": deque(maxlen=MAX_QUEUE_LENGTH),
+        ">0.5μm": deque(maxlen=MAX_QUEUE_LENGTH),
+        ">1.0μm": deque(maxlen=MAX_QUEUE_LENGTH),
+        ">2.5μm": deque(maxlen=MAX_QUEUE_LENGTH),
+        ">5.0μm": deque(maxlen=MAX_QUEUE_LENGTH),
+        ">10μm": deque(maxlen=MAX_QUEUE_LENGTH),
+    }
+    PARTICLE_SIZES = list(particle_counts.keys())
+
+    # Initialize a deque to store a 10 minutes window with PM2.5 readings and their timestamps
+    pm_timestamps = deque()
+    pm_readings = deque()
+
+    def __init__(self):
+        self._lock = Lock()
+        serial_port = AirQualityUtils.find_serial_port()
+        self._pt = plantower.Plantower(serial_port)
+
+        if self.ENABLE_ACTIVE_MODE:
+            print("Making sure the sensor is correctly setup for active mode. Please wait...")
+            #make sure it's in the correct mode if it's been used for passive beforehand
+            #Not needed if freshly plugged in
+            self._pt.mode_change(plantower.PMS_ACTIVE_MODE) #change back into active mode
+            self._pt.set_to_wakeup() #ensure fan is spinning
+            time.sleep(self.WAKEUP_DELAY_SEC) # give it a chance to stabilise
+
+            new_serial_port = AirQualityUtils.find_serial_port()
+            if new_serial_port != serial_port:
+                self._pt = plantower.Plantower(new_serial_port)
+
+    def add_pm25_reading(self, current_time, value):
+        with self._lock:
+            self.pm_timestamps.append(current_time)
+            self.pm_readings.append(value)
+
+            # Remove readings older than 10 minutes (600 seconds)
+            while self.pm_timestamps and current_time - self.pm_timestamps[0] > timedelta(seconds=self.MEASUREMENT_WINDOW_LENGTH_SEC):
+                self.pm_timestamps.popleft()
+                self.pm_readings.popleft()
+
+    def calculate_nowcast_aqi(self):
+        with self._lock:
+            if len(self.pm_readings) < 2:
+                return None
+
+            # Calculate the range and scaled rate of change
+            min_pm = min(self.pm_readings)
+            max_pm = max(self.pm_readings)
+            range_pm = max_pm - min_pm
+            scaled_rate_of_change = range_pm / max_pm if max_pm != 0 else 0
+
+            # Calculate weight factor
+            weight_factor = max(1 - scaled_rate_of_change, 0.5)
+
+            # Calculate weighted average concentration
+            weighted_sum = 0
+            weight_sum = 0
+            for i, (pm, time) in enumerate(zip(self.pm_readings, self.pm_timestamps)):
+                time_diff = (self.pm_timestamps[-1] - time).total_seconds() / self.MEASUREMENT_WINDOW_LENGTH_SEC  # Normalize to 10 minutes
+                weight = weight_factor ** time_diff
+                weighted_sum += pm * weight
+                weight_sum += weight
+
+            nowcast_concentration = weighted_sum / weight_sum
+
+            for bp_low, bp_high, aqi_low, aqi_high in self.BREAKPOINTS:
+                if bp_low <= nowcast_concentration <= bp_high:
+                    aqi = ((aqi_high - aqi_low) / (bp_high - bp_low)) * (nowcast_concentration - bp_low) + aqi_low
+                    return round(aqi, 1)
+
+        return None  # If concentration is out of range
+    
+    @staticmethod
+    def find_serial_port():
+        ports = list(serial.tools.list_ports.comports())
+        for port in ports:
+            if 'ttyACM' in port.device:
+                return port.device
+        print('No serial port')
+        sys.exit(1)
+
+    @staticmethod
+    def aqi_category(aqi):
+        if 0 <= aqi:
+            if aqi <= 50:
+                return 'Good'
+            if aqi <= 100:
+                return 'Moderate'
+            if aqi <= 150:
+                return 'Unhealthy for Sensitive Groups'
+            if aqi <= 200:
+                return 'Unhealthy'
+            if aqi <= 300:
+                return 'Very Unhealthy'
+            if aqi <= 500:
+                return 'Hazardous'
+        return 'Out of range {aqi}'
+
+    def read_sample(self):
+        sample = self._pt.read()
+        self.sample_count += 1
+
+        # Append new data to the lists
+        self.time_data.append(sample.timestamp)
+
+        self.pm1_cf1.append(sample.pm10_cf1)
+        self.pm2_5_cf1.append(sample.pm25_cf1)
+        self.pm10_cf1.append(sample.pm100_cf1)
+
+        self.pm1_std.append(sample.pm10_std)
+        self.pm2_5_std.append(sample.pm25_std)
+        self.pm10_std.append(sample.pm100_std)
+
+        self.particle_counts[">0.3μm"].append(sample.gr03um)
+        self.particle_counts[">0.5μm"].append(sample.gr05um)
+        self.particle_counts[">1.0μm"].append(sample.gr10um)
+        self.particle_counts[">2.5μm"].append(sample.gr25um)
+        self.particle_counts[">5.0μm"].append(sample.gr50um)
+        self.particle_counts[">10μm"].append(sample.gr100um)
+
+        # update PM data for AQI computation
+        self.add_pm25_reading(sample.timestamp, sample.pm25_cf1)
