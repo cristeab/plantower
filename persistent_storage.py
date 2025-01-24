1f27c27a6084d13f60438636d4e1636469610b4b
Bogdan Cristea <cristeab@gmail.com>
2025-01-24 15:34:54 +0200

    persistent storage class corrections

--- persistent_storage.py
+++ persistent_storage.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 import influxdb_client, os, time
-from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
+from influxdb_client.client.exceptions import InfluxDBError
 from datetime import datetime, timezone
 
 
@@ -13,8 +14,8 @@ class PersistentStorage:
     aqi_bucket="aqi"
 
     def __init__(self):
-        self._token = os.environ.get("INFLUXDB_TOKEN")
-        self._write_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+        token = os.environ.get("INFLUXDB_TOKEN")
+        self._write_client = influxdb_client.InfluxDBClient(url=self.url, token=token, org=self.org)
         self._write_api = self._write_client.write_api(write_options=SYNCHRONOUS)
 
     def write_pm(self, sample):
@@ -35,7 +36,17 @@ class PersistentStorage:
             .field("gr50um", sample.gr50um)
             .field("gr100um", sample.gr100um)
         )
-        self._write_api.write(bucket=self.pm_bucket, org=self.org, record=point)
+        try:
+            self._write_api.write(bucket=self.pm_bucket, org=self.org, record=point)
+        except InfluxDBError as e:
+            if e.response.status == 401:
+                print(f"Authentication error: {e}")
+            elif e.response.status == 404:
+                print(f"Bucket or organization not found: {e}")
+            else:
+                print(f"An error occurred while writing data: {e}")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
 
     def write_aqi(self, timestamp, pm25_cf1_aqi):
         utc_timestamp = timestamp.astimezone(timezone.utc)
@@ -44,4 +55,14 @@ class PersistentStorage:
             .time(utc_timestamp)
             .field("pm25_cf1_aqi", pm25_cf1_aqi)
         )
-        self._write_api.write(bucket=self.aqi_bucket, org=self.org, record=point)
+        try:
+            self._write_api.write(bucket=self.aqi_bucket, org=self.org, record=point)
+        except InfluxDBError as e:
+            if e.response.status == 401:
+                print(f"Authentication error: {e}")
+            elif e.response.status == 404:
+                print(f"Bucket or organization not found: {e}")
+            else:
+                print(f"An error occurred while writing data: {e}")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
