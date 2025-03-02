e3d501b9df24c71fe60cd549e12145967c453570
Bogdan Cristea <cristeab@gmail.com>
2025-03-02 11:50:46 +0200

    refactoring

--- persistent_storage.py
+++ persistent_storage.py
@@ -13,8 +13,6 @@ import json
 class PersistentStorage:
     org = "home"
     url = "http://localhost:8086"
-    pm_bucket="pm"
-    aqi_bucket="aqi"
 
     class Bucket(Enum):
         PM = "pm"
@@ -33,7 +31,7 @@ class PersistentStorage:
     def _verify_token(self):
         try:
             test_point = Point("test").field("test_field", 1)
-            self._write_api.write(bucket=self.pm_bucket, org=self.org, record=test_point)
+            self._write_api.write(bucket=self.Bucket.PM.value, org=self.org, record=test_point)
             print("Token verification successful.")
         except InfluxDBError as e:
             print(f"Token verification failed: {e}")
@@ -41,6 +39,19 @@ class PersistentStorage:
         except Exception as e:
             print(f"An unexpected error occurred during token verification: {e}")
             sys.exit(1)
+
+    def _write(self, bucket: Bucket, point: Point):
+        try:
+            self._write_api.write(bucket=bucket.value, org=self.org, record=point)
+        except InfluxDBError as e:
+            if e.response.status == 401:
+                self._logger.error(f"Authentication error: {e}")
+            elif e.response.status == 404:
+                self._logger.error(f"Bucket or organization not found: {e}")
+            else:
+                self._logger.error(f"An error occurred while writing data: {e}")
+        except Exception as e:
+            self._logger.error(f"An unexpected error occurred: {e}")
 
     def write_pm(self, i, sample):
         point = (
@@ -59,17 +70,7 @@ class PersistentStorage:
             .field("gr50um", sample.gr50um)
             .field("gr100um", sample.gr100um)
         )
-        try:
-            self._write_api.write(bucket=self.pm_bucket, org=self.org, record=point)
-        except InfluxDBError as e:
-            if e.response.status == 401:
-                self._logger.error(f"Authentication error: {e}")
-            elif e.response.status == 404:
-                self._logger.error(f"Bucket or organization not found: {e}")
-            else:
-                self._logger.error(f"An error occurred while writing data: {e}")
-        except Exception as e:
-            self._logger.error(f"An unexpected error occurred: {e}")
+        self._write(self.Bucket.PM, point)
 
     def write_aqi(self, timestamp, pm25_cf1_aqi):
         point = (
@@ -77,20 +78,9 @@ class PersistentStorage:
             .time(timestamp)
             .field("pm25_cf1_aqi", pm25_cf1_aqi)
         )
-        try:
-            self._write_api.write(bucket=self.aqi_bucket, org=self.org, record=point)
-        except InfluxDBError as e:
-            if e.response.status == 401:
-                self._logger.error(f"Authentication error: {e}")
-            elif e.response.status == 404:
-                self._logger.error(f"Bucket or organization not found: {e}")
-            else:
-                self._logger.error(f"An error occurred while writing data: {e}")
-        except Exception as e:
-            self._logger.error(f"An unexpected error occurred: {e}")
+        self._write(self.Bucket.AQI, point)
 
-    def read(self, bucket: Bucket, i: int):
-        query = f'from(bucket:"{bucket.value}") |> range(start: -1m) |> filter(fn: (r) => r._measurement == "air_quality_data_{i}") |> last()'
+    def _read(self, query):
         tables = self._write_client.query_api().query(query)
         data = {}
         for table in tables:
@@ -99,9 +89,19 @@ class PersistentStorage:
                 data[record.get_field()] = record.get_value()
         return json.dumps(data)
 
+    def read_pm(self, i: int):
+        query = f'from(bucket:"{self.Bucket.PM.value}") |> range(start: -1m) |> filter(fn: (r) => r._measurement == "air_quality_data_{i}") |> last()'
+        return self._read(query)
+
+    def read_aqi(self):
+        query = f'from(bucket:"{self.Bucket.AQI.value}") |> range(start: -1m) |> filter(fn: (r) => r._measurement == "air_quality_data") |> last()'
+        return self._read(query)
+
 
 if __name__ == "__main__":
     storage = PersistentStorage()
     for i in range(2):
-        data = storage.read(PersistentStorage.Bucket.PM, i)
+        data = storage.read_pm(i)
         print(data)
+    data = storage.read_aqi()
+    print(data)
