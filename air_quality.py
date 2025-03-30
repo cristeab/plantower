0ad98c2747724227cfbdcf07aadec00f7ca04846
Bogdan Cristea <cristeab@gmail.com>
2025-03-30 20:55:58 +0300

    add print_utils module

--- air_quality.py
+++ air_quality.py
@@ -5,14 +5,7 @@
 
 from air_quality_utils import AirQualityUtils
 import argparse
-import sys
-
-
-def clear_lines(n):
-    for _ in range(n):
-        sys.stdout.write('\033[F')  # Move cursor up one line
-        sys.stdout.write('\033[K')  # Clear the line
-    sys.stdout.flush()
+from print_utils import clear_lines
 
 
 parser = argparse.ArgumentParser(description='Script to read air quality data from multiple serial ports')
