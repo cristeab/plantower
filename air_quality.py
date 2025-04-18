28354ebea130a727452d7d0ae487698f71cd1271
Bogdan Cristea <cristeab@gmail.com>
2025-04-18 21:51:53 +0300

    corrected data printing

--- air_quality.py
+++ air_quality.py
@@ -40,11 +40,12 @@ try:
             once = False
         else:
             clear_lines(aq_utils._serial_port_count + 1)
+
         # print data
         print(f'{aq_utils.aqi} | {aq_utils.elapsed_time} | Samples {aq_utils.sample_count} | Rel. err. {aq_utils.sensors_relative_error_percent}% | Spearman corr. {aq_utils.sensors_spearman_corr}%')
         for i in range(aq_utils._serial_port_count):
             line = f'#{i} '
-            line += f'PM1.0: {aq_utils._pm10_cf1[i][-1]} ug/m3, PM2.5: {aq_utils._pm2_5_cf1[i][-1]} ug/m3, PM10: {aq_utils._pm10_cf1[i][-1]} ug/m3'
+            line += f'PM1.0: {aq_utils._pm1_cf1[i][-1]} ug/m3, PM2.5: {aq_utils._pm2_5_cf1[i][-1]} ug/m3, PM10: {aq_utils._pm10_cf1[i][-1]} ug/m3'
             line += ' | '
             line += f'Particles in 0.1L of air: >0.3um {aq_utils._gr03um[i][-1]}, >0.5um {aq_utils._gr05um[i][-1]}, >10um {aq_utils._gr10um[i][-1]}, >25um {aq_utils._gr25um[i][-1]}, >50um {aq_utils._gr50um[i][-1]}, >100um {aq_utils._gr100um[i][-1]}'
             print(line)
